a44f570bb64c5ba3c98e1504dbbbd7098a5f5bcc
tran son <newexpress3d@gmail.com>
2026-08-18 17:25:43 +0700

    refactor(theme): extract theme setup bootstrap

--- functions.php
+++ functions.php
@@ -9,48 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Register theme support.
- */
-function drthai_health_setup() {
-	add_theme_support( 'wp-block-styles' );
-	add_theme_support( 'responsive-embeds' );
-	add_theme_support( 'editor-styles' );
-	add_editor_style( 'style.css' );
-	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'automatic-feed-links' );
-	add_theme_support( 'title-tag' );
-	add_theme_support(
-		'html5',
-		array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' )
-	);
-}
-add_action( 'after_setup_theme', 'drthai_health_setup' );
-
-/**
- * Load the front-end stylesheet.
- */
-function drthai_health_enqueue_styles() {
-	$theme = wp_get_theme();
-	wp_enqueue_style(
-		'drthai-health-style',
-		get_stylesheet_uri(),
-		array(),
-		$theme->get( 'Version' )
-	);
-}
-add_action( 'wp_enqueue_scripts', 'drthai_health_enqueue_styles' );
-
-/**
- * Register the theme pattern category.
- */
-function drthai_health_register_pattern_category() {
-	register_block_pattern_category(
-		'drthai-health',
-		array( 'label' => __( 'DrThai Health', 'drthai-health' ) )
-	);
-}
-add_action( 'init', 'drthai_health_register_pattern_category' );
+require_once __DIR__ . '/inc/theme-setup.php';
 
 /**
  * Register private callback requests in the dashboard.
